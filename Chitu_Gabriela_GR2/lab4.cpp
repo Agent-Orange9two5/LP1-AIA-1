59a710184f13cb64a52d3fa394498d070b4613f2
gabrielachitu29 <147593753+gabrielachitu29@users.noreply.github.com>
2023-10-18 11:09:26 +0300

    Update lab4.cpp

--- Chitu_Gabriela_GR2/lab4.cpp
+++ Chitu_Gabriela_GR2/lab4.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int v[10], i , s;
+
+ for(int i=0; i<=10; i++) {
+    cin>>v[i];
+    if(v[i]%2!=0) s=s+v[i];
+ }
+ cout<<"Suma numerelor impare este:"<<s;
+ cout <<"Numerele pare sunt: ";
+ for(i=0; i<=10; i++) {
+        if(v[i]%2==0) cout<<v[i]<<" ";
+ }
+ cout<<"Vectorul inversat este: ";
+    for(i=9; i<=0; i++) {
+    cout<<v[i]<<" ";
+    }
+
+return 0;
+ }
+
 
