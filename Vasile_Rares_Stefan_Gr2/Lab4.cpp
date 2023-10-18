3e96c903ec2fc43b6123a40ae06641653d8d4d6b
Sfet17 <147593842+Sfet17@users.noreply.github.com>
2023-10-18 10:47:48 +0300

    Update Lab4.cpp

--- Vasile_Rares_Stefan_Gr2/Lab4.cpp
+++ Vasile_Rares_Stefan_Gr2/Lab4.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,v[10],s=0;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+        if(v[i]%2)
+            s=s+v[i];
+        if(v[i]%2==0)
+            cout<<v[i]<<' ';
+    }
+        cout<<endl;
+        cout<<s<<endl;
+    for(int i=n-1; i>=0; i--)
+        cout<<v[i]<<' ';
+    return 0;
+}
 
