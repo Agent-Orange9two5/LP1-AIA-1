af63c345781e9cc2c3777aad5b8c7d856e66f65a
RusuDenice <147593775+RusuDenice@users.noreply.github.com>
2023-10-18 10:50:48 +0300

    Update lab4.cpp

--- Rusu_Denice_Grupa2/lab4.cpp
+++ Rusu_Denice_Grupa2/lab4.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int v[10],si=0;
+    for(int i=0;i<10;i++)
+    {
+        cin>>v[i];
+        if(v[i]%2!=0)si+=v[i];
+    }
+    cout<<"Suma nr impare este: "<<si<<endl;
+    cout<<"Numerele pare sunt: "<<endl;
+    for(int i=0;i<10;i++)
+    {
+        if(v[i]%2==0)cout<<v[i]<<" ";
+    }
+    cout<<endl<<"Vectorul inversat este: "<<endl;
+    for(int i=9;i>=0;i--)
+    {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
