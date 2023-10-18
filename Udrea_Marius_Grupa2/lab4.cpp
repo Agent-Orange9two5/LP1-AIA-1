3a56dd146ef52f61ff3d56ce53a5eb5e661638bc
udrea-zis-beleaua <147593815+udrea-zis-beleaua@users.noreply.github.com>
2023-10-18 10:55:43 +0300

    Update lab4.cpp

--- Udrea_Marius_Grupa2/lab4.cpp
+++ Udrea_Marius_Grupa2/lab4.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	int v[]={1,2,3,4,5,6,7,8,9,10}, sumimp,i;
+	for (i = 0; i < 10; i++) {
+		if (v[i] % 2 == 1) {
+			sumimp = v[i] + sumimp;
+		}
+		if (v[i] % 2 == 0) {
+			cout << v[i] << " ";
+		}
+	}
+	cout<<'\n';
+	cout << "suma nr impare:" << sumimp<<'\n';
+	cout << "vector inversat:\n";
+	for (i = 9;i >= 0; i--) {
+		cout << v[i] << " ";
+	}
+}
