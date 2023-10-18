65293951026bd113bff98a733c54f1b9b4d2265b
teodora-marinescu <147584317+teodora-marinescu@users.noreply.github.com>
2023-10-18 08:51:15 +0300

    Update Laborator_4.cpp

--- Marinescu_Teodora_G1/Laborator_4.cpp
+++ Marinescu_Teodora_G1/Laborator_4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=10;
+    int v[n], si=0;
+    cout<<"nr pare :";
+    for(int i=0;i<n;i++)
+    {
+        v[i]=i+1;
+        if(v[i]%2==1) si+=v[i];
+        else cout<<v[i]<<" ";
+    }
+    cout<<endl<<"suma nr impare : "<< si<< endl;
+    for(int i=n-1;i>=0;i--) cout<<v[i]<<" ";
+
+
+
+    return 0;
+}
 
