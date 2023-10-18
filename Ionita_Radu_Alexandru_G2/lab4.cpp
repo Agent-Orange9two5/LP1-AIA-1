97e95a52d7d39300662220b9d8d2b467fede639c
Ionita Radu Alexandru <44524490+AkroDogy@users.noreply.github.com>
2023-10-18 10:53:17 +0300

    Update lab4.cpp

--- Ionita_Radu_Alexandru_G2/lab4.cpp
+++ Ionita_Radu_Alexandru_G2/lab4.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main()
 {
-    int V[10],i,suimp=0;
+    int V[10],i,suimp=0,pare;
     for (i=0; i<=9; i++) {
         cin>>V[i];
         if (V[i]%2==1)
             suimp=suimp+V[i];
+    }
+    cout<<endl<<"Nr Pare: ";
+    for (i=0; i<=9; i++) {
         if (V[i]%2==0)
-            cout<<endl<<"Nr Pare: "<<V[i]<<" "<<endl;
+            cout<<V[i]<<" ";
     }
     cout<<"Suma nr impare: "<<suimp<<endl;
     cout<<"Vectorul inversat este: ";
@@ -19,4 +22,3 @@ int main()
 
     return 0;
 }
-
