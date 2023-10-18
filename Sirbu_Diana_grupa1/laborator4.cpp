307c75d513adbf0bf3eb8a876597c7d21128992d
Diana12345677 <147583975+Diana12345677@users.noreply.github.com>
2023-10-18 09:21:33 +0300

    Update laborator4.cpp

--- Sirbu_Diana_grupa1/laborator4.cpp
+++ Sirbu_Diana_grupa1/laborator4.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[10],suma=0,n=10;
+    cout<<"introduceti v:";
+    for(int i=0;i<n;i++)
+    {
+
+        cin>>v[i];
+        if(v[i]%2!=0)
+            suma+=v[i];
+    }
+    cout<<"suma nr impare= "<<suma<<endl;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i]%2==0)
+        cout<<"nr pare= "<<v[i]<<endl;
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+      cout<<"vectorul inversat"<<v[i];
+    }
+    return 0;
+}
