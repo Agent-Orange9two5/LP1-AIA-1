f9f8140aa181c73956301ffdb7ffdf85ed2f3c75
EneIonut09 <147584002+EneIonut09@users.noreply.github.com>
2023-10-18 09:27:20 +0300

    Update Laborator4.cpp

--- Ene_Ionut_Grupa1/Laborator4.cpp
+++ Ene_Ionut_Grupa1/Laborator4.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int v[10],i,s;
+        cout<<"Elementele vectorului sunt: ";
+        for (i=0; i<10; i++) cin>>v[i];
+        for (i=0; i<10; i++) if (v[i]%2==1) s=s+v[i];
+
+        cout<<"Suma cifrelor impare este: "<<s<<endl;
+        cout<<"Numerele pare sunt: ";
+        for (i=0; i<10; i++){
+            if (v[i]%2==0)
+                cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        cout<<"Vectorul inversat este: ";
+        for (i=9; i>=0; i--){
+            cout<<v[i]<<" ";
+        }
+
+return 0;
+}
 
