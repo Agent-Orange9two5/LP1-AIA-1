b31f3d09f09fa4fce7eb572899406ab3aa0828ad
Glont7 <147593860+Glont7@users.noreply.github.com>
2023-10-18 10:48:37 +0300

    Update Lab4.cpp

--- Glont_Tudor_G2/Lab4.cpp
+++ Glont_Tudor_G2/Lab4.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+/*suma nr imp
+nr pare
+vectorul inversat*/
+using namespace std;
+
+int main()
+{
+    int v[10], n, i, s=0;
+    cin>>n;
+    for(i=1; i<=n;i++)
+       {
+           cin>>v[i];
+           if(v[i]%2==1)
+                s=s+v[i];
+       }
+       cout<<s<<endl;
+    for(i=1;i<=n;i++)
+    {
+        if(v[i]%2==0)
+            cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    for(i=n;i>=1;i--)
+        cout<<v[i]<<" ";
+
+    return 0;
+}
 
