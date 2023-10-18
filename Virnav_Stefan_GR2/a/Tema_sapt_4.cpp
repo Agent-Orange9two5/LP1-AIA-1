c6e9e968acd00f93a48acf3fddafe3c7605ddcc1
StefanVir <122097429+StefanVir@users.noreply.github.com>
2023-10-18 12:02:30 +0300

    Delete Virnav_Stefan_GR2/a directory

--- Virnav_Stefan_GR2/a/Tema_sapt_4.cpp
+++
@@ -1,8 +0,0 @@
-/*
-Se citeste o matrice 4x4 nr reale sa se afis nr neg din amtrice si poz lor
-Sa se afis taote elem deasupra diag secundare
-Toate elm de sub diag principala
-Toate nr palindrome de pe linile impare
-Sa se creeze 4 vectori:
-  Fiecare vector memoreaza fiecare triunghi format dintre cele 2 diagonale sec si principala
-*/
