0effd3fa2c2c2e95a5ffaa16a750c59d32b3e98d
CrisTutu14 <147583964+CrisTutu14@users.noreply.github.com>
2023-10-18 08:54:04 +0300

    Update Laborator4.cpp

--- Tutu_Cristian_G1/Laborator4.cpp
+++ Tutu_Cristian_G1/Laborator4.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[10],i,s=0;
+    for(i=0;i<10;i++)
+    {
+        cout<<"v["<<i<<"]"<<" = ";
+        cin>>v[i];
+    }
+    cout<<"Numerele pare sunt: ";
+    for(i=0;i<10;i++)
+    {
+        if(v[i]%2==0)
+            cout<<v[i]<<" ";
+        else s+=v[i];
+    }
+    cout<<endl;
+    cout<<"Suma numerelor impare este "<<s<<endl;
+    cout<<"Vectorul inversat: ";
+    for(i=9;i>=0;i--)
+        cout<<v[i]<<" ";
+    return 0;
+}
 
