d2866cbe27a1e47b2b50352707804e952caf4cc0
Bogdan11231 <147583899+Bogdan11231@users.noreply.github.com>
2023-10-18 08:55:49 +0300

    Update laborator4.cpp

--- Bucura_Bogdan_Gr1/laborator4.cpp
+++ Bucura_Bogdan_Gr1/laborator4.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+using namespace std;
+int i,s,k;
+int main(){
+int v[]={1,2,3,4,5,6,7,8,9,10};
+for(i=0;i<=9;i++){
+    if (v[i]%2==1)
+    s=s+v[i];
+}
+for(i=9;i>=0;i--)
+cout<<v[i]<<"; ";
+cout<<endl<<"numerele pare sunt:";
+for(i=0;i<=9;i++){
+    if (v[i]%2==0)
+    cout<<v[i]<<"; ";
+}
+cout<<endl<<"S impare este:"<<s;
+return 0;
+}
