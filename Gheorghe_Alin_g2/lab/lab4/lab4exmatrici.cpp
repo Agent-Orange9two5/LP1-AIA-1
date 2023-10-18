6313f27dfcf44223b7eab7c14c2d508a52f7d5b3
AlinGheorghe7 <147593812+AlinGheorghe7@users.noreply.github.com>
2023-10-18 11:55:37 +0300

    Update lab4exmatrici.cpp

--- Gheorghe_Alin_g2/lab/lab4/lab4exmatrici.cpp
+++ Gheorghe_Alin_g2/lab/lab4/lab4exmatrici.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+
+using namespace std;
+//sa se afiseze elem de pe diag princ, elem de sub diag sec,si suma nr de 3 cif
+int main()
+{
+    int a[5][5],i,j,s=0,n;
+    cin>>n;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+            cin>>a[i][j];
+    }
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+            {
+                if(i==j)
+                {
+                    cout<<endl<<a[i][j]<<" ";
+                }
+                if(i+j>n-1&&i>j)
+                {
+                    cout<<a[i][j];
+                }
+                if(a[i][j]>99&&a[i][j]<1000)
+                {
+                    s=s+a[i][j];
+                }
+            }
+    }
+    cout<<s;
+    return 0;
+}
 
