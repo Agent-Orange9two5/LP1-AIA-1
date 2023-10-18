16ab6e3d8625f9cc0ab3f8069cd744461e782e5a
dianabianca28 <147593927+dianabianca28@users.noreply.github.com>
2023-10-18 10:51:20 +0300

    Update lab4.cpp
    adaugat cod

--- Balan_Bianca_GR2/lab4.cpp
+++ Balan_Bianca_GR2/lab4.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int v[10],s=0,i,np=0,inv;
+for(i=0;i<10;i++)
+{
+    cin>>v[i];
+    if(v[i]%2==1)
+        s=s+v[i];
+        else
+        cout<<v[i]<<" ";
+
+
+}
+for (i=9;i>=0;i--)
+    cout<<endl<<v[i];
+cout<<endl<<"suma este"<<s;
+return 0;
+
+}
 
