7117004627ce54ec687f5a34b1cfc816df040ab4
VladMarius544 <147593871+VladMarius544@users.noreply.github.com>
2023-10-18 11:01:43 +0300

    Update lab4.cpp

--- Vlad_Marius_Grupa2/lab4.cpp
+++ Vlad_Marius_Grupa2/lab4.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,v[10],i,s=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>v[i];
+        if(v[i]%2==1)
+            s=s+v[i];
+        else
+            cout<<v[i]<<' ';
+
+    }
+    cout<<endl;
+    cout<<s<<endl;
+    for(i=n-1;i>=0;i--)
+        cout<<v[i]<<' ';
+
+    return 0;
+}
