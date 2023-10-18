f8a338f761ea8c2e6c865c23647d48ddf03bff21
adaonu17 <147583916+adaonu17@users.noreply.github.com>
2023-10-18 09:25:11 +0300

    Update laborator4.cpp

--- Onu_Ada_gr1/laborator4.cpp
+++ Onu_Ada_gr1/laborator4.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int v[10],n=10, suma=0;
+   for (int i=0; i<n; i++)
+   {
+       cout<<"introduceti v: ";
+       cin>>v[i];
+       if(v[i]%2!=0)
+        suma+=v[i];
+
+   }
+    cout<<"suma nr impare= "<<suma<<endl;
+    for (int i=0; i<n; i++)
+    {
+
+     if (v[i]%2==0)
+        cout<<"nr pare= "<<v[i]<<endl;
+
+    }
+    for (int i=n-1; i>=0; i--)
+    {
+        cout<< v[i];
+    }
+
+    return 0;
+}
 
