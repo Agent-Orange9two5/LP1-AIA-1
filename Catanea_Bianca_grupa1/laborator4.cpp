341c084663c12b82799961a92dbe6e23bddca8ee
Cătănea Bianca-Florina <147583988+cataneabianca@users.noreply.github.com>
2023-10-18 09:08:41 +0300

    Update laborator4.cpp

--- Catanea_Bianca_grupa1/laborator4.cpp
+++ Catanea_Bianca_grupa1/laborator4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+   int v[10], i, suma=0 ;
+   cout << " Numerele pare sunt: " ;
+   for ( i = 0 ; i < 10 ; i++ )
+     {
+         v[i] = i+1;
+
+            if ( v[i] % 2 == 0)
+         cout << " " << v[i] ;
+       }
+       cout << endl;
+   for ( i = 0 ; i < 10; i++ )
+        if ( v[i] % 2 == 1)
+            suma+=v[i];
+   cout << "Suma numerelor impare este: " << " " << suma << endl;
+
+    cout << " Vectorul inversat este: " << " ";
+   for ( i = 9 ; i >= 0; i--)
+    cout << v[i] << " ";
+
+
+    return 0;
+}
