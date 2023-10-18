42b945508de5c2a94355f9eb4e06f4298c0887cf
Constantin Mihai-Valentin <62659070+mihai14feb@users.noreply.github.com>
2023-10-18 11:49:17 +0300

    Create lab4.cpp

---
+++ Constantin_Mihai_G2/Laboratore/lab4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[4][4];
+    int s = 0;
+    cout << "Citeste matricea:\n";
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            cin >> a[i][j];
+    cout << "\nElementele de pe diagonala principala: ";
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (i == j)
+                cout << a[i][j] << " ";
+    cout << "\nElementele de sub diagonala secundara: ";
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (i + j >= 4)
+                cout << a[i][j] << " ";
+    cout << "\nSuma elementelor de 3 cifre: ";
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (a[i][j] > 99)
+                s += a[i][j];
+    cout << s;
+}
