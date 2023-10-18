f0578321d35c20c1ff2d146747402b93974ff268
Alex-Micu <147685702+Alex-Micu@users.noreply.github.com>
2023-10-18 09:49:23 +0300

    Update Laborator4.cpp

--- Micu_Alexandru_Gr 1/Laborator4.cpp
+++ Micu_Alexandru_Gr 1/Laborator4.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n,v[10],i,s=0;
+cin>>n;
+for(i=0;i<0;i++)
+    cin>>v[i];
+while(n!=0){
 
+
+    if(n%2==0)
+    s=s+n%10;
+n=n/10;}
+cout<<s;
+
+return 0;
+
+
+
+
+}
