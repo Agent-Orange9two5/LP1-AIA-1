c6586c3cee39977f5d57f21921a7e92614fe34b0
Popescu-Tudor-Stefan <147593940+Popescu-Tudor-Stefan@users.noreply.github.com>
2023-10-18 11:00:21 +0300

    Update Lab 4 .cpp

--- Popescu_Tudor_Stefan/Lab 4 .cpp
+++ Popescu_Tudor_Stefan/Lab 4 .cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+int a[10]={1,2,3,4,5,6,7,8,9,10},s=0,i;
+for(i=0;i<10;i++)
+{
+    if(a[i]%2==1)
+        s=s=a[i];
+        if(a[i]%2==0)
+     cout<<a[i]<<" ";
+}for(i=9;i>=0;i--)
+cout<<a[i]<< " ";
+cout<<s;
+ return 0;
+}
