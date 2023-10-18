202400273b90f1ec63f5461c1ed6be6b844c7056
Bektaș Denis <147593858+Denniz263@users.noreply.github.com>
2023-10-18 10:47:45 +0300

    Update Laborator4.cpp
    Suma nr impare

--- Bektas_Denis_G2/Laborator4.cpp
+++ Bektas_Denis_G2/Laborator4.cpp
@@ -1,1 +1,18 @@
+include <iostream>
 
+using namespace std;
+
+int main()
+{
+int v[10], s=0, i;
+for (i=0; i<=9; i++){
+cin>>v[i];
+
+if (v[i]%2==1)
+s= s+v[i];
+
+if (v[i]%2==0)
+    cout<<v[i];}
+cout<<"Suma nr impare: "<<s<<endl;
+    return 0;
+}
