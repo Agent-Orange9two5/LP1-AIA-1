7818ea86e55c712f0067c8eb7729b1de5e14952d
AlinGheorghe7 <147593812+AlinGheorghe7@users.noreply.github.com>
2023-10-18 10:47:34 +0300

    lab4 ex1

--- Gheorghe_Alin_g2/lab.cpp
+++ Gheorghe_Alin_g2/lab.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[10],s=0,i;
+    for(i=0;i<=9;i++) { cin>>v[i];
+    if(v[i]%2==1) s=s+v[i];
+    if(v[i]%2==0) cout<<endl<<"Nr pare "<<v[i]<<endl;
+    }
+    for(i=9;i>=0;i--){ cout<<v[i]<<" "; }
+    cout<<"suma "<<s;
+
+    return 0;
+}
 
