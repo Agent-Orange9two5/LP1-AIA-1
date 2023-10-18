1ec8a18879b8a981a98b1af71cad9696a5c9f70f
raducr516 <147583097+raducr516@users.noreply.github.com>
2023-10-18 08:52:07 +0300

    Update laborator4.cpp

--- Radu_Cristian_Grupa1/laborator4.cpp
+++ Radu_Cristian_Grupa1/laborator4.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int v[10],i,j,n,s1=0,s2=0;
+   cin>>n;
+   for(i=0;i<n;i++)
+    cin>>v[i];
+   for(i=0;i<n;i++)
+    if(v[i]%2!=0)
+     s1=s1+v[i];
+     else
+        s2=s2+v[i];
+     cout<<s2<<" " <<s1<<endl;
+     for(i=0;i<n;i++)
+     {
+         for(j=0;j<n-i-1;j++)
+            swap(v[j],v[j+1]);
+     }
+     for(i=0;i<n;i++)
+        cout<<v[i]<<" ";
+    return 0;
+}
 
