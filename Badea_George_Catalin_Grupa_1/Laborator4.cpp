3d44145f35c3eabd8dff9dc1eff923a614012a51
BadeaGeorgeCatalinB13 <147584145+BadeaGeorgeCatalinB13@users.noreply.github.com>
2023-10-18 08:54:35 +0300

    Update Laborator4.cpp

--- Badea_George_Catalin_Grupa_1/Laborator4.cpp
+++ Badea_George_Catalin_Grupa_1/Laborator4.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v[10],i,s=0,n;
+    for(i=0;i<10;i++)
+        cin>>v[i];
+    for(i=0;i<10;i++)
+    {
+        if(v[i]%2==1)
+            {
+                s=s+v[i];
+            }
+        else
+            {
+                cout<<v[i]<<" ";
+                cout<<endl;
+            }
+    }
+    for(i=9;i>=0;i--)
+        cout<<v[i]<<" ";
+    cout<<s;
+    return 0;
+}
 
