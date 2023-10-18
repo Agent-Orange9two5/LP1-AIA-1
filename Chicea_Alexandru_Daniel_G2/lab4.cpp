0867feb3fd5b1fd2c06656464ac56f3796ecc955
alexandruchicea <147593875+alexandruchicea@users.noreply.github.com>
2023-10-18 10:47:56 +0300

    Update lab4.cpp
    Vectori

--- Chicea_Alexandru_Daniel_G2/lab4.cpp
+++ Chicea_Alexandru_Daniel_G2/lab4.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+int v[10], i, j, s;
+int main()
+{
+    for(i=0;i<10;i++)
+    {
+        v[i]=i+1;
+        if(v[i]%2!=0)
+            s=s+v[i];
+    }
+    cout<<"Suma numerelor impare:"<<s<<endl;
+    cout<<"Nr pare:";
+    for(i=0;i<10;i++)
+    {
+        if(v[i]%2==0)
+            cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    for(i=9;i>=0;i--)
+        cout<<v[i]<<" ";
+    return 0;
+}
