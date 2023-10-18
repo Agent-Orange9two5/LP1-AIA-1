14e512b1eb1ba917b46a5837c8e1634c27a7469d
GeorgeDinu22 <147584158+GeorgeDinu22@users.noreply.github.com>
2023-10-18 08:59:17 +0300

    Update Laborator_4.cpp

--- Dinu_George_Grupa1/Laborator_4.cpp
+++ Dinu_George_Grupa1/Laborator_4.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{int v[10],n,i,s=0,k=0,a[10];
+cout<<"Introduceti numarul de elemente al vetorului:";
+cin>>n;
+cout<<endl;
+cout<<"Introduceti elementele vectorului:";
+for(i=0;i<n;i++)
+    cin>>v[i];
+for(i=0;i<n;i++)
+    if (v[i]%2!=0)
+    s=s+v[i];
+for(i=n-1;i>=0;i--)
+   {
+
+    a[k]=v[i];
+    k++;
+}
+cout<<"Vectorul inversat este:";
+for(i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    cout<<endl;
+    cout<<"Suma numerelor impare este:"<<s;
+    cout<<endl;
+    cout<<"Numerele pare sunt:";
+    for(i=0;i<n;i++)
+        if (v[i]%2==0)
+    cout<<v[i]<<" ";
+
+    return 0;
+}
