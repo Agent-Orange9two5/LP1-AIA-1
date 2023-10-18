6df7de8994a00c3ff2588938e9e139c83f1fe9a5
StefanVir <122097429+StefanVir@users.noreply.github.com>
2023-10-18 10:47:30 +0300

    Update lab4.cpp
    adaugat cod

--- Virnav_Stefan_GR2/lab4.cpp
+++ Virnav_Stefan_GR2/lab4.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    int v[10],s=0,i;
+    for(i=0;i<10;i++)
+        {
+            cin>>v[i];
+            if(v[i]%2==1)
+                s+=v[i];
+            else
+                cout<<v[i]<<" e par"<<endl;
+        }
+    for(i=9;i>=0;i--)
+        cout<<endl<<v[i];
+    cout<<endl<<"suma este:"<<s;
+    return 0;
+}
