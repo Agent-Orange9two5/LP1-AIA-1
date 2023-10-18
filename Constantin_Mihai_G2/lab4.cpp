d2ca705574066efd0ac387d816feacffc0b1ff2c
Constantin Mihai-Valentin <62659070+mihai14feb@users.noreply.github.com>
2023-10-18 10:40:20 +0300

    Update lab4.cpp

--- Constantin_Mihai_G2/lab4.cpp
+++ Constantin_Mihai_G2/lab4.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int v[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int s = 0;
+    for (auto i: v)
+        if (i % 2 != 0)
+            s += i;
+    cout << "Suma nr impare: " << s << "\n" << "Nr pare: ";
+    for (auto i: v)
+        if (i % 2 == 0)
+            cout << i << " ";
+    cout << "\nVectorul inersat: ";
+    for (int i = 9; i >= 0; i--)
+        cout << v[i] << " ";
+}
 
