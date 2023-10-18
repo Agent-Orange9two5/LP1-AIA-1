51440ca9f01837789d74fffd7c474311ff997c75
AndreeaPufulete <147594046+AndreeaPufulete@users.noreply.github.com>
2023-10-18 11:55:15 +0300

    Update matrice.cpp

--- Pufulete_Andreea_grupa2/laborator 4/matrice.cpp
+++ Pufulete_Andreea_grupa2/laborator 4/matrice.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a[4][4],n,i,j;
+cin>>n;
+cout<<matricea este:<<" ";
+for (i=1;i<=n;i++)
+for(j=1;j<=n;j++) cin>>a[i][j];
+    cout<<"elementele de pe diagonala principala sunt :"
+for (i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+        if(a[i][j]>99 s=s+a[i][j]; cout <<"suma numerelor de 3 cifre este"<<s<<" ";
+        if (i==j) cout <<"elementele de pe diagonala principala sunt;"<<a[i][j];
+               else if(i+j>n+1) cout <<" elementele de sub diagonala secundara sunt:"<<a[i][j]<<" ";
+                return o
+        }
 
