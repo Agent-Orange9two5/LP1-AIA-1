82dbd1a4678b0826a3cb5ce8f8ab27d1677ccb4f
SaucaVictor <147583973+SaucaVictor@users.noreply.github.com>
2023-10-18 08:51:01 +0300

    Update laborator4.cpp

--- Sauca_Victor_Gr1/laborator4.cpp
+++ Sauca_Victor_Gr1/laborator4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int v[]={1,2,3,4,5,6,7,8,9,10},s=0;
+    for(int i=0;i<10;++i){
+        if(v[i]%2)s+=v[i];
+    }
+    cout<<"Suma nr impare: "<<s<<endl;
+    for(int i=0;i<10;++i){
+        if(v[i]%2==0)cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    int j=9;
+    for(int i=0;i<5;++i){
+        swap(v[i],v[j]);
+        j--;
+    }
+    for(int i=0;i<10;++i){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
 
