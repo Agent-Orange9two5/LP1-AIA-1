c738c8bb5aea5be788f2a3c80f66cbe2e7654d12
AndreeaPufulete <147594046+AndreeaPufulete@users.noreply.github.com>
2023-10-18 10:52:15 +0300

    Update Lab4.cpp

--- Pufulete_Andreea_grupa2/Lab4.cpp
+++ Pufulete_Andreea_grupa2/Lab4.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int v[10],i,s=0,n;
+cin>>n;
+for(i=1;i<=n;i++)
+cin>>v[i];
+for(i=1;i<=n;i++)
+    if(v[i]%2==1) s=s+v[i];
+cout<<"suma numerelor impare este:"<<s<<" ";
+cout<< "numerele pare sunt:";
+for(i=1;i<=n;i++)
+    if(v[i]%2==0) cout<<v[i]<<" ";
+ cout<<"vectorul inversat este:";
+ for (i=n;i>=1;i--)cout<<v[i]<<" ";
+
+    return 0;
+}
 
