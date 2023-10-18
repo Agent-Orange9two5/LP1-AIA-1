e9902eb9d0e0e458188053771e393d6726f6e11c
TamasagIrina <147584133+TamasagIrina@users.noreply.github.com>
2023-10-18 09:01:14 +0300

    Add files via upload

---
+++ Tamasag_Irina_grupa1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[10], si=0, n, i;
+    cout<<"introduceti marimea vectorului";
+    cin>>n;
+    cout<<"introduceti vectorul";
+
+    for(i=0;i<n;i++)
+
+        cin>>v[i];
+
+    cout<<"numerele pare sunt";
+      for(i=0;i<n;i++)
+      {
+          if(v[i]%2==0)
+            cout<<v[i];
+            else si= si+v[1];
+      }
+    cout<<endl;
+    cout<<"suma nr impare este: "<<si<<endl;
+    cout<<"vectorul inversat este:";
+    for(i=n-1;i>=0;i--)
+        cout<<v[i];
+    return 0;
+}
