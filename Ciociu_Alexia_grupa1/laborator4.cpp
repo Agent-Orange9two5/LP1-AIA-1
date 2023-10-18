88639b01022a32bf8a01c0280ac092d2166003f9
alexiaciociu <147584134+alexiaciociu@users.noreply.github.com>
2023-10-18 09:08:01 +0300

    Update laborator4.cpp

--- Ciociu_Alexia_grupa1/laborator4.cpp
+++ Ciociu_Alexia_grupa1/laborator4.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+using namespace std;
+int n,v[10],i,s=0,nrpare=0;
+int main()
+{ cout<<"Introduceti numarul de elemente:";
+    cin>>n;
+    cout<<"Vectorul are elementele:";
+for(i=0;i<n;i++) cin>>v[i];
+for(i=0;i<n;i++)
+    if(v[i]%2!=0)  s+=v[i];
+                else nrpare++;
+    cout<<"Suma numerelor impare este:"<<s<<endl;
+    cout<<"Numerele pare sunt:"<<nrpare<<endl;
+cout<<"Vectorul inversat este:";
+for(i=n-1;i>=0;i--)
+    cout<<v[i]<<" ";
+    return 0;
+}
