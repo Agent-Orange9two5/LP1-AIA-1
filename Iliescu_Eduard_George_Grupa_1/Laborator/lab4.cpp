e3395d2b4e2918ab5629d4e29b47ac21da736f98
Iliescu Eduard George <46160402+ed0one@users.noreply.github.com>
2023-10-21 22:06:16 +0300

    Update lab4.cpp

--- Iliescu_Eduard_George_Grupa_1/Laborator/lab4.cpp
+++ Iliescu_Eduard_George_Grupa_1/Laborator/lab4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int v[10],i,s=0;
+    for(i=0;i<10;i++)
+        cin>>v[i];
+        for(i=0;i<10;i++)
+            {if(v[i]%2==1)
+                s+=v[i];
+            if(v[i]%2==0)
+                cout<<v[i]<<" ";
+            }
+
+        cout<<endl;
+        for(i=9;i>=0;i--)
+            cout<<v[i]<<" ";
+            cout<<endl;
+            cout<<s;
+    return 0;
+}
