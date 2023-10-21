610e7ecba02e31921eded4df1a85792b32a2b2f0
DariusSauciuc26 <147584199+DariusSauciuc26@users.noreply.github.com>
2023-10-21 14:23:48 +0300

    Update Laborator4.cpp

--- Sauciuc_Darius_G1/Laborator/Laborator4.cpp
+++ Sauciuc_Darius_G1/Laborator/Laborator4.cpp
@@ -1,1 +1,49 @@
+/*
+1 2 3 4 5 6 7 8 9 10
 
+afisati:
+    suma nr impare
+    nr pare
+    vectorul inversat
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[10];
+    int i, s = 0;
+
+    cout << "Introduceti numerele din vector: ";
+    for(i = 0; i < 10; i++)
+    {
+        cin >> v[i];
+    }
+
+    for(i = 0; i < 10; i++)
+    {
+        if(v[i] % 2 != 0)
+        {
+            s = s + v[i];
+        }
+    }
+    cout << "Suma nr impare este: " << s << endl;
+
+    for(i = 0; i < 10; i++)
+    {
+        if(v[i] % 2 == 0)
+        {
+            cout << v[i] << " este nr par" << "; ";
+        }
+    }
+    cout << endl;
+
+    cout << "Vectorul inversat este: ";
+
+    for(i = 9; i >= 0; i--)
+    {
+        cout << v[i] << " ";
+    }
+}
