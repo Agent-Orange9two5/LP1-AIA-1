ee1b1edd0068f5b63c7e016d4e85c5e63f209091
StefanVir <122097429+StefanVir@users.noreply.github.com>
2023-10-23 23:07:17 +0300

    Update tema_sapt_4.cpp
    Finalized the program

--- Virnav_Stefan_GR2/Teme/tema_sapt_4.cpp
+++ Virnav_Stefan_GR2/Teme/tema_sapt_4.cpp
@@ -25,27 +25,74 @@ bool palindrom(int n)
     }
 int main()
 {
- int v[4][4],i,j,a[2],;
+ int v[4][4],i,j,a[2],b[2],c[2],d[2],k=0;
+
+
  for(i=0;i<4;i++)
     for(j=0;j<4;j++)
         {
             cin>>v[i][j];
         }
+
+
     cout<<"Elemente deasupra diagonalei secundare:";
     for(i=0;i<4;i++)
         for(j=0;j<4;j++)
             if(j<3-i)
                 cout<<v[i][j]<<" ";
+
+
     cout<<endl<<"Elemente sub diagonala principala:";
     for(i=0;i<4;i++)
         for(j=0;j<4;j++)
             if(i>j)
                 cout<<v[i][j]<<" ";
+
+
     cout<<endl<<"Elemente palindroam de pe linii impare:";
     for(i=0;i<4;i++)
         for(j=0;j<4;j++)
-            if(i%2==0&& palindrom(v[i][j])==true)
+            if(i%2==0 && palindrom(v[i][j])==true)
                 cout<<v[i][j]<<" ";
-
+//Am zis sa fac cu formule ca rezolvarea sa fie mai generalizata chiar daca pe 4x4 puteam sa o fac cu volori direte la fiecare triunghi
+    //Triunghiul din stanga
+    cout<<endl<<"Elemente din triunghiul stanga:";
+    for(i=0;i<4;i++)
+        for(j=0;j<4;j++)
+            if(i>j && j<3-i)
+                {
+                    cout<<v[i][j]<<" ";
+                    a[k]=v[i][j];
+                    k++;
+                }
+    k=0;
+    cout<<endl<<"Elemente din triunghiul sus:";
+    for(i=0;i<4;i++)
+        for(j=0;j<4;j++)
+            if(i<j && j<3-i)
+                {
+                    cout<<v[i][j]<<" ";
+                    b[k]=v[i][j];
+                    k++;
+                }
+    k=0;
+    cout<<endl<<"Elemente din triunghiul dreapta:";
+    for(i=0;i<4;i++)
+        for(j=0;j<4;j++)
+            if(i<j && j>3-i)
+                {
+                    cout<<v[i][j]<<" ";
+                    b[k]=v[i][j];
+                    k++;
+                }
+    cout<<endl<<"Elemente din triunghiul jos:";
+    for(i=0;i<4;i++)
+        for(j=0;j<4;j++)
+            if(i>j && j>3-i)
+                {
+                    cout<<v[i][j]<<" ";
+                    b[k]=v[i][j];
+                    k++;
+                }
  return 0;
 }
