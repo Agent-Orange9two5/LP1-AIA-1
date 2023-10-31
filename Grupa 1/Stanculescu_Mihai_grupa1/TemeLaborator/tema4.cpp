e698ead0e1f94dad088c7cabcb96d7d7ac6bb4db
Eduard Varlan <testifyats@gmail.com>
2023-10-31 22:47:00 +0200

    Weekly check

--- Grupa 1/Stanculescu_Mihai_grupa1/TemeLaborator/tema4.cpp
+++ Grupa 1/Stanculescu_Mihai_grupa1/TemeLaborator/tema4.cpp
@@ -1,129 +1,187 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int i,j,n,m[100][100],nr=0,v[100],suma=0,ogl=0,cln,n1,tr1[100],tr2[100],tr3[100],tr4[100],cnt=0;
-    cout<<"Introduceti numarul de linii si coloane al matricii:";
-    cin>>n;
-    n1=n;
-    cout<<"Introduceti termenii din matrice:"<<endl;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cin>>m[i][j];
+int main()
+{
+    int i, j, n, m[100][100], nr = 0, v[100], suma = 0, ogl = 0, cln, n1, tr1[100], tr2[100], tr3[100], tr4[100], cnt = 0;
+    cout << "Introduceti numarul de linii si coloane al matricii:";
+    cin >> n;
+    n1 = n;
+    cout << "Introduceti termenii din matrice:" << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cin >> m[i][j];
         }
     }
-    cout<<"Elementele de pe diagonala principala sunt:";
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(i==j){
-                cout<<m[i][j]<<" ";
-            }   
+    cout << "Elementele de pe diagonala principala sunt:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                cout << m[i][j] << " ";
+            }
         }
     }
-    cout<<endl;
-    cout<<"Elementele de pe diagonala secundara sunt:";
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(i+j==n-1){
-                v[nr]=m[i][j];
+    cout << endl;
+    cout << "Elementele de pe diagonala secundara sunt:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i + j == n - 1)
+            {
+                v[nr] = m[i][j];
                 nr++;
-            }   
-        }
-}
-while(n1-1>=0){
-    cout<<v[nr-1]<<" ";
-    nr--;
-    n1--;
-}
-cout<<endl;
-cout<<"suma elementelor de deasupra diagonalei principale este:";
-for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-        if(i<j){
-            suma +=m[i][j];
-        }
-    }
-}
-cout<<suma;
-
-cout<<endl;
- cout<<"Numerele palindrom de pe diagonala principala sunt:";
-    for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-        if(i==j){
-            cln=m[i][j];
-            while(cln!=0){
-                ogl=ogl*10 +cln%10;
-                cln=cln/10;
-            }
-            if(ogl==m[i][j]){
-                cout<<m[i][j]<<" ";//nu inteleg dc nu merge
-                ogl=0;
-                cln=0;
             }
         }
     }
-}
-cout<<endl;
-cout<<"Numerele din triunghiul de sus sunt:";
- for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-        if(i<j && i+j <n-1){
-            tr1[cnt]=m[i][j];
-            cnt++;
+    while (n1 - 1 >= 0)
+    {
+        cout << v[nr - 1] << " ";
+        nr--;
+        n1--;
+    }
+    cout << endl;
+    cout << "suma elementelor de deasupra diagonalei principale este:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i < j)
+            {
+                suma += m[i][j];
+            }
         }
     }
- }
- for(i=0;i<cnt;i++){
-    cout<<tr1[i]<<" ";
- }
- cnt=0;
- cout<<endl;
-cout<<"Numerele din triunghiul din dreapta sunt:";
- for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-if(i<j && i+j>n-1){
-    tr2[cnt]=m[i][j];
-    cnt++;
-}
-}
- }
+    cout << suma;
 
-for(i=0;i<cnt;i++){
-    cout<<tr2[i]<<" ";
- }
- cnt=0;
-cout<<endl;
-
-cout<<"Numerele din triunghiul de jos sunt:";
- for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-if(i>j && i+j>n-1){
-    tr3[cnt]=m[i][j];
-    cnt++;
-}
-}
- }
- for(i=0;i<cnt;i++){
-    cout<<tr3[i]<<" ";
- }
- cnt=0;
-cout<<endl;
- 
- cout<<"Numerele din triunghiul din stanga sunt:";
- 
-  for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-        if(i>j && i+j<n-1){
-            tr4[cnt]=m[i][j];
-            cnt++;
+    cout << endl;
+    cout << "Numerele palindrom de pe diagonala principala sunt:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                cln = m[i][j];
+                while (cln != 0)
+                {
+                    ogl = ogl * 10 + cln % 10;
+                    cln = cln / 10;
+                }
+                if (ogl == m[i][j])
+                {
+                    cout << m[i][j] << " "; // nu inteleg dc nu merge
+                    ogl = 0;
+                    cln = 0;
+                }
+            }
         }
-}
-  }
+    }
+    cout << endl;
+    cout << "Numerele din triunghiul de sus sunt:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i < j && i + j < n - 1)
+            {
+                tr1[cnt] = m[i][j];
+                cnt++;
+            }
+        }
+    }
+    for (i = 0; i < cnt; i++)
+    {
+        cout << tr1[i] << " ";
+    }
+    cnt = 0;
+    cout << endl;
+    cout << "Numerele din triunghiul din dreapta sunt:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i < j && i + j > n - 1)
+            {
+                tr2[cnt] = m[i][j];
+                cnt++;
+            }
+        }
+    }
 
-for(i=0;i<cnt;i++){
-    cout<<tr4[i]<<" ";
- }
+    for (i = 0; i < cnt; i++)
+    {
+        cout << tr2[i] << " ";
+    }
+    cnt = 0;
+    cout << endl;
 
-  return 0;
+    cout << "Numerele din triunghiul de jos sunt:";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i > j && i + j > n - 1)
+            {
+                tr3[cnt] = m[i][j];
+                cnt++;
+            }
+        }
+    }
+    for (i = 0; i < cnt; i++)
+    {
+        cout << tr3[i] << " ";
+    }
+    cnt = 0;
+    cout << endl;
+
+    cout << "Numerele din triunghiul din stanga sunt:";
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i > j && i + j < n - 1)
+            {
+                tr4[cnt] = m[i][j];
+                cnt++;
+            }
+        }
+    }
+
+    for (i = 0; i < cnt; i++)
+    {
+        cout << tr4[i] << " ";
+    }
+
+    return 0;
 }
+
+
+/* Pentru linia 78: unde ai zis ca nu merge, actually merge bine. Un caz concret de testare: 
+
+Introduceti numarul de linii si coloane al matricii:4
+Introduceti termenii din matrice:
+121 5 6 9
+541 555 22 1
+698 214 989 5
+3 6 9 12
+Elementele de pe diagonala principala sunt:121 555 989 12
+Elementele de pe diagonala secundara sunt:3 214 22 9
+suma elementelor de deasupra diagonalei principale este:48
+Numerele palindrom de pe diagonala principala sunt:121 555 989
+Numerele din triunghiul de sus sunt:5 6
+Numerele din triunghiul din dreapta sunt:1 5
+Numerele din triunghiul de jos sunt:6 9
+Numerele din triunghiul din stanga sunt:541 698
+Process returned 0 (0x0)   execution time : 49.346 s
+Press any key to continue.
+
+
+Deci este in regula, programul merge bine. 
+*/
--- Grupa 2/Nica_Andrei_Gr2/teme/tema 1.cpp
+++ Grupa 2/Nica_Andrei_Gr2/teme/tema 1.cpp
@@ -82,3 +82,107 @@ for(i=0;i<8;i++)cout<<v[i]<<" ";
 /*salut chiar nu pot sa inteleg de ce nu mi se citesc Numerele
 dintre diagonale si cele negative (voi veni cu intrebari la laborator)
 */
+
+
+/*  Conditiile si acoladele nu sunt puse tocmai bine. Incearca sa lucrezi putin mai ordonat si sa rezolvi fiecare cerinta rand pe rand. 
+un exemplu de rezolvare corecta ar fi: 
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Input
+    int a[4][4],n=4,i,j,m,k,v[100],h=0,va[2],vb[2],vc[2],vd[2],t=0, u=0,x=0,z=0;
+    cout<<">> Introduceti numerele pentru matrice: "<<endl;
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++)
+            cin>>a[i][j];
+    }
+    system("cls");
+
+    //Matrice output
+    cout<<">> Matricea este: <<"<<endl;
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++)
+            cout<<a[i][j]<<" ";
+            cout<<endl;
+    }
+
+    //Numere de deasupra diag sec
+    cout<<endl<<">> Numerele de deasupra diagonala secundara sunt: ";
+    for(i=0;i<n;i++) {
+      for(j=0;j<n;j++) {
+        if (i+j!=n-1 && i+j<=2 )
+            cout<<a[i][j]<<" ";
+      }
+    }
+
+    //Numere de sub diag prin
+    cout<<endl<<">> Numerele de sub diagonala principala sunt: ";
+    for(i=0;i<n;i++) {
+      for(j=0;j<n;j++) {
+        if (i!=j && i>j)
+            cout<<a[i][j]<<" ";
+      }
+    }
+
+    //Numere neg si poz lor
+    cout<<endl<<">> Numere negative si pozitia lor: "<<endl;
+    for(i=0;i<n;i++) {
+      for(j=0;j<n;j++) {
+        if (a[i][j] < 0) {
+            cout<<" Numarul [ "<<a[i][j]<<" ]"<<" (Coloana: "<<j+1<<" si Linia: "<<i+1<<")"<<endl;
+        }
+      }
+    }
+
+    //Palindrome
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(i%2==1)
+                k=a[i][j];
+                m=0;
+                if(a[i][j]<-9 || a[i][j]>9){
+                while(k){
+                    m=m*10+k%10;
+                    k/=10;
+                    }
+                if(m==a[i][j]) v[h++]=m;
+                }
+        }
+    }
+    cout<<endl<<">> Vector cu numerele palindrome din matrice cu linile impare: ";
+    for(i=0;i<h;i++) cout<<v[i]<<" ";
+
+    //Vectori cu numerele dintre diagonale
+    for(i=0;i<n;i++){
+        for (j=0 ; j<n; j++) {
+            if (i<j && i+j<3)
+                va[t++] = a[i][j];
+            if (j<i && i+j<3)
+                vb[u++] = a[i][j];
+            if (i<j && i+j>=4)
+                vc[x++] = a[i][j];
+            if (i>j && i+j>=4)
+                vd[z++] = a[i][j];
+        }
+    }
+    cout<<endl<<endl<<">> Vector 1: ";
+    for (i=0;i<2;i++){
+        cout<<va[i]<<" ";}
+    cout<<endl<<">> Vector 2: ";
+    for (i=0;i<2;i++){
+        cout<<vb[i]<<" ";}
+    cout<<endl<<">> Vector 3: ";
+    for (i=0;i<2;i++){
+        cout<<vc[i]<<" ";}
+    cout<<endl<<">> Vector 4: ";
+    for (i=0;i<2;i++){
+        cout<<vd[i]<<" "; }
+    return 0;
+}
+*/
