9b9bb74c5447c9d3d7e0adede7deebdc73732e58
crist1923 <148947329+crist1923@users.noreply.github.com>
2023-11-14 16:05:24 +0200

    Add files via upload

---
+++ Grupa 2/Ghetau_Cristian_G2/Laboratoare/lab7pb1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+int x,y;
+}Point;
+
+typedef struct {
+ Point topLeft;
+ Point bottomRight;
+ }Rectangle;
+
+int main()
+{
+    Point p1,p2;
+    p1.x =0;
+    p1. y= 3;
+    p2.x = 4;
+    p2. y= 0;
+    printf("p1 la (%d, %d)\n", p1.x, p1.y);
+    printf("p2 la (%d, %d)\n", p2.x, p2.y);
+    Rectangle rect;
+    rect.topLeft = p1;
+    rect.bottomRight= p2;
+    printf("Stanga sus la (%d,%d)\n", rect.topLeft.x, rect.topLeft.y);
+    printf("Dreapta jos la (%d,%d)\n", rect.bottomRight.x, rect.bottomRight.y);
+    return 0;
+}
---
+++ Grupa 2/Ghetau_Cristian_G2/Laboratoare/lab7pb2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct stud
+{
+    char nume[50];
+    int nota1, nota2;
+    float nota_ex, med;
+};
+
+int main()
+{
+    stud student[30], temp;
+    int n, i, ok;
+    cout<<"Introduceti numarul de studenti: ";
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cout<<"Introduceti numele studentului: ";
+        cin>>student[i].nume;
+        cout<<"Introduceti nota 1: ";
+        cin>>student[i].nota1;
+        cout<<"Introduceti nota2 2: ";
+        cin>>student[i].nota2;
+        cout<<"Introduceti nota examen: ";
+        cin>>student[i].nota_ex;
+        student[i].med=(student[i].nota1+student[i].nota2)/2*0.4+0.6*student[i].nota_ex;
+    }
+    cout<<"Sortare alfabetica: "<<endl;
+    do
+    {
+        ok=1;
+        for(i=0; i<n-1; i++)
+            if(strcmp(student[i].nume, student[i+1].nume)>0)
+            {
+                temp=student[i];
+                student[i]=student[i+1];
+                student[i+1]=temp;
+                ok=0;
+            }
+    }
+    while(ok==0);
+    for(i=0; i<n; i++)
+        cout<<student[i].nume<<" "<<student[i].med<<endl;
+    cout<<"Sortare dupa medie: "<<endl;
+    do
+    {
+        ok=1;
+        for(i=0; i<n-1; i++)
+            if(student[i].med>student[i+1].med)
+            {
+                temp=student[i];
+                student[i]=student[i+1];
+                student[i+1]=temp;
+                ok=0;
+            }
+    }while(ok==0);
+    for(i=0;i<n;i++)
+        cout<<student[i].nume<<" "<<student[i].med<<endl;
+    return 0;
+}
