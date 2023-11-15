428e65abe3ad02d2e72c6375c50e24402bb2ad24
Iustin Mărgărit <147593770+Iustin-Margarit@users.noreply.github.com>
2023-11-15 10:40:50 +0200

    Create lab8.c

---
+++ Grupa 2/Margarit_Iustin_Gr2/Laboratoare/lab8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    float *a,x;
+    int n,i;
+    printf("Cate numere?");
+    scanf("%d", &n);
+
+    a=(float *) malloc(n * sizeof(float));
+    if(!a)
+        printf("Nu pot aloca memorie\n");
+
+for(i=0;i<n;i++){
+    printf("a[%d]:", i);
+    scanf("%f",&a[i]);}
+
+    printf("x:");
+    scanf("%f", &x);
+
+
+    printf("Numerele din intervalul (%.2f. %.2f) sunt", x-5, x+5);
+    for(i=0;i<n;i++)
+        if(fabs(x-a[i])<5)
+           printf("%.2f",a[i]);
+
+    free(a);
+
+
+    return 0;
+}
