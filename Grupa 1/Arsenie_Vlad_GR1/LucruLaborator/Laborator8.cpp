7119d1cff06e8f2e82b92a26746607ed93f59415
Arsenie-Vlad <147584006+Arsenie-Vlad@users.noreply.github.com>
2023-11-15 09:38:57 +0200

    Update Laborator8.cpp

--- Grupa 1/Arsenie_Vlad_GR1/LucruLaborator/Laborator8.cpp
+++ Grupa 1/Arsenie_Vlad_GR1/LucruLaborator/Laborator8.cpp
@@ -31,7 +31,7 @@ int main()
     free(a);
     return 0;
 }
-*/
+
 #include <iostream>
 #include <stdlib.h>
 
@@ -176,4 +176,34 @@ int main()
 
     return 0;
 }
+*/
+#include <stdio.h>
 
+int functie1()
+{
+    static int x=0;
+    x+=10;
+    return x;
+}
+int functie2()
+{
+    int x=0;
+    x+=10;
+    return x;
+}
+
+void main()
+{
+    int n=10;
+    printf("Functie cu variabila static: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ", functie1());
+    }
+    printf("\n");
+    printf("Functie cu variabila non-static: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ", functie2());
+    }
+}
