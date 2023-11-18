24e98f3c620a8f2f1d47062e4ad52d3bafe808dc
Bektaș Denis <147593858+Denniz263@users.noreply.github.com>
2023-11-18 19:41:30 +0000

    Create lab8c.cpp

---
+++ Grupa 2/Bektas_Denis_G2/Teme/lab8c.cpp
@@ -0,0 +1,89 @@
+//Sa se explice succint scopul programului de  mai jos si sa se corecteze erorile de sintaxa si logica.
+
+#include <iostream> /*  < librarie lipsa */
+#include <string.h> /*  < librarie lipsa */
+#include <cstdlib>
+#include <cstring>
+
+int main() {
+    int size;
+    std::cout << "Enter the initial size of the array: ";
+    std::cin >> size;
+    std::cin.ignore();
+
+
+    char **arrMalloc = (char **)malloc(size * sizeof(char *));
+    if (arrMalloc == nullptr) {
+        std::cout << "Memory allocation failed using malloc!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Memory allocated using malloc." << std::endl;
+
+
+    std::cout << "Enter " << size << " strings:" << std::endl;
+    for (int i = 0; i < size; ++i) { /* am modificat i < size din i <= size */
+        const int bufferSize = 100;
+        arrMalloc[i] = (char *)malloc(bufferSize * sizeof(char));
+        if (arrMalloc[i] == nullptr) {
+            std::cout << "Memory allocation failed for string " << i << " using malloc!" << std::endl;
+
+            for (int j = 0; j < i; ++j) {
+                free(arrMalloc[j]);
+            }
+            free(arrMalloc); /* ; lipsa dupa functie */
+            return 1;
+        }
+        std::cin.getline(arrMalloc[i], bufferSize);
+    }
+
+    int newSize;
+    std::cout << "Enter the new size for reallocation: ";
+    std::cin >> newSize;
+    std::cin.ignore();
+
+
+    char **arrRealloc = (char **)realloc(arrMalloc, newSize * sizeof(char *));
+    if (arrRealloc == nullptr) {
+        std::cout << "Memory reallocation failed using realloc!" << std::endl;
+
+        for (int i = 1; i < size; ++i) {
+            free(arrMalloc[i]);
+        }
+        free(arrMalloc);
+        return 1;
+    }
+
+    std::cout << "Memory reallocated using realloc." << std::endl;
+
+    std::cout << "Enter " << newSize - size << " more strings:" << std::endl;
+    for (int i = size; i < newSize; ++i) {
+        const int bufferSize = 100;
+        arrRealloc[i] = (char *)malloc(bufferSize * sizeof(char));
+        if (arrRealloc[i] == nullptr) { /* variabila lipsa pentru realloc */
+            std::cout << "Memory allocation failed for string " << i << " using malloc!" << std::endl;
+
+            for (int j = 0; j < i; ++j) { /* ; lipsa in for */
+                free(arrRealloc[j]);
+            }
+            free(arrRealloc);
+            return 1;
+        }
+        std::cin.getline(arrRealloc[i], bufferSize);
+    }
+
+
+    std::cout << "Updated array of strings:" << std::endl;
+    for (int i = 0; i < newSize; ++i) {
+        std::cout << arrRealloc[i] << std::endl;
+    }
+
+    for (int i = 0; i <= newSize; ++i) {
+        free(arrRealloc[i]);
+    }
+    free(arrRealloc);
+
+    return 0;
+}
+/* programul este asemanator celui de la 1b, doar ca in acest program putem utiliza si siruri de caractere
+Se introduce o dimensiune pentru vectorul initial, apoi elemntele sale, dupa care este reinlocuit cu un vector nou, unde se introduc si elemntele noi */
