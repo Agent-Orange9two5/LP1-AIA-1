0c3af3b1287532cdcc818e7ac7a26157a8a9d23e
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-11-20 01:02:56 +0200

    Update vectori.cpp

--- Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_4/vectori.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_4/vectori.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v[10],i,s=0,n;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cin>>v[i];
+        if(v[i]%2==1)
+            s=s+v[i];
+        else
+            cout<<v[i]<< " ";
+    }
+    cout<<endl;
+    cout<<"Suma= "<<s<<endl;
+    for(i=n-1; i>=0; i--)
+    {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
