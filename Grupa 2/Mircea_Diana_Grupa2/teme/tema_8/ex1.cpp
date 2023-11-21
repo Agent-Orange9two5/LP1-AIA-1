5897a898a541b7bcba2c667a3bf6e2ea16eacf1a
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-11-21 10:39:26 +0200

    Update ex1.cpp

--- Grupa 2/Mircea_Diana_Grupa2/teme/tema_8/ex1.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/teme/tema_8/ex1.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <cstdlib>
 
+int main() {
+    int size;
+    std::cout << "Enter the size of the array: ";//lipsea ":"
+    std::cin >> size;
+
+    int *arrMalloc = static_cast<int *>(malloc(size * sizeof(int)));
+    if (arrMalloc == nullptr) {
+        std::cout << "Memory allocation failed using malloc!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Memory allocated using malloc." << std::endl;
+
+
+    int *arrCalloc = static_cast<int *>(calloc(size, sizeof(int)));//liosea "*"
+    if (arrCalloc == nullptr) {
+        std::cout << "Memory allocation failed using calloc!" << std::endl;
+        free(arrMalloc);
+        return 1;
+    }
+
+    std::cout << "Memory allocated using calloc." << std::endl;
+
+
+    int newSize;
+    std::cout << "Enter the new size for reallocation: ";
+    std::cin >> newSize;
+
+
+    int *arrRealloc = static_cast<int *>(realloc(arrMalloc, newSize * sizeof(int)));
+    if (arrRealloc = nullptr) {
+        std::cout << "Memory reallocation failed using realloc!" << std::endl;
+        free(arrMalloc);
+        free(arrCalloc);
+        return 1;
+    }
+
+    std::cout << "Memory reallocated using realloc." << std::endl;
+
+    free(arrRealloc);
+    free(arrCalloc);//lipsea ";"
+
+    return 0;
+}
+//
