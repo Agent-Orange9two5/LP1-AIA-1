0873a87312b21a81137f1db152015fef35b7913a
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-11-22 11:13:53 +0200

    Update towerofhanoi.cpp

--- Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_9/towerofhanoi.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_9/towerofhanoi.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+void towerofhanoi(int n, char source, char auxiliary, char destination)
+{
+    if(n==1)
+    {
+        cout<<"Move disk 1 from "<<source<< " to "<<destination<<endl;
+        return;
+    }
+    towerofhanoi(n-1,source,destination,auxiliary);
+    cout<<"Move disk "<<n<<" from "<<source<<" to "<<destination<<endl;
+    towerofhanoi(n-1,auxiliary,source,destination);
+}
+int main()
+{
+    int num_disks=3;
+    towerofhanoi(num_disks,'A','B','C');
+    return 0;
+}
