5028ed625c35b09170ed7ef6509477f70912ff40
RusuDenice <147593775+RusuDenice@users.noreply.github.com>
2023-11-22 11:39:02 +0200

    Create problema3.cpp

---
+++ Grupa 2/Rusu_Denice_Grupa2/Laboratoare/Lab9/problema3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+int i = 0;
+void foo1(int);
+void foo2(int);
+void foo1(int n)
+{
+    if (i <= n)
+    {
+        cout << i << " ";
+        i++;
+        foo2(n);
+    }
+    else
+    {
+        return;
+    }
+}
+void foo2(int n)
+{
+    if (i <= n)
+    {
+        cout << i << " ";
+        i++;
+        foo1(n);
+    }
+    else
+    {
+        return;
+    }
+}
+int main(void)
+{
+    int n;
+    cin >> n;
+    foo1(n);
+    return 0;
+}
