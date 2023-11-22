ac6c4441854597aa08f70f96edc6990be32c8ffa
alexiaciociu <147584134+alexiaciociu@users.noreply.github.com>
2023-11-22 08:50:02 +0200

    Update Lab9.cpp

--- Grupa 1/Ciociu_Alexia_grupa1/laborator/Lab9.cpp
+++ Grupa 1/Ciociu_Alexia_grupa1/laborator/Lab9.cpp
@@ -34,4 +34,38 @@ if(n<=1) return 1;
   int main(){
   int num=5;
   cout<<fa(num);
-  return 0;}
+  return 0;} 
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int i=0;
+void foo1(int);
+void foo2(int);
+void foo1(int n){
+if(i<=n)
+   {
+   cout<<i<<" ";
+   i++;
+   foo2(n);}
+   else return ;
+}
+void foo2(int n){
+if(i<=n)
+    {
+        cout<<i<<" ";
+        i++;
+        foo1(n);}
+        else return ;
+
+    }
+int main(){int n;
+cin>>n;
+foo1(n);
+return 0;}
+
