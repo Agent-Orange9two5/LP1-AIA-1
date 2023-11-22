8f01db37ff823d703416ad1b8c57722c0ac20628
DariusSauciuc26 <147584199+DariusSauciuc26@users.noreply.github.com>
2023-11-22 08:51:22 +0200

    Update Laborator9-pb1.cpp

--- Grupa 1/Sauciuc_Darius_G1/Laborator/Laborator9-pb1.cpp
+++ Grupa 1/Sauciuc_Darius_G1/Laborator/Laborator9-pb1.cpp
@@ -1,1 +1,35 @@
+//calculul lui n!
+#include <iostream>
+
+using namespace std;
+
+int fact_iterativ(int n)
+{
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+int fact_recursiv(int n)
+{
+    //Cazul de baza pentru recursivitate: 0! = 1
+    if(n == 0)
+        return 1;
+    else
+        //Formula recursiva: n! = n * (n - 1)
+        return n * fact_recursiv(n - 1);
+}
+int main()
+{
+    int n;
+    cout << "Dati n: ";
+    cin >> n;
+    //Afisarea rezultatului pentru metoda iterativa
+    cout << "n! cu metoda iterativa este egal cu: " << fact_iterativ(n) << endl;
+    //Afisarea rezultatului pentru metoda recursiva
+    cout << "n! cu metoda recursiva este egal cu: " << fact_recursiv(n);
+    return 0;
+}
 
