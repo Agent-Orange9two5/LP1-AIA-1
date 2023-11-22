91c8f22a01a6d715e7c87fe71988e2a2ab6a8a14
RusuDenice <147593775+RusuDenice@users.noreply.github.com>
2023-11-22 11:43:16 +0200

    Create problema5.cpp

---
+++ Grupa 2/Rusu_Denice_Grupa2/Laboratoare/Lab9/problema5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void fib(int n, int a, int b)
+{
+    if (n == 2)
+        return;
+    else
+    {
+        cout << a + b << " ";
+        return fib(n - 1, b, a + b);
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "1" << " 1 ";
+    fib(n, 1, 1);
+    return 0;
+}
