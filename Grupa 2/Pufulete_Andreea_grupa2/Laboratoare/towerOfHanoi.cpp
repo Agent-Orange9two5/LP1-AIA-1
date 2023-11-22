a9b35164d49d2d27ef4b62a3e90048287baa44bd
AndreeaPufulete <147594046+AndreeaPufulete@users.noreply.github.com>
2023-11-22 11:15:09 +0200

    Create towerOfHanoi.cpp

---
+++ Grupa 2/Pufulete_Andreea_grupa2/Laboratoare/towerOfHanoi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void towrOfHanoi (int n,char source,char auxiliary ,char destination){
+if (n==1){
+    cout<<"move disk 1 from"<<source<<" to "<<destination<<endl;
+    return;
+}
+towerOfHanoi(n-1,source,destination,auxiliary);
+cout<<"move disk"<<n<<" from "<<source<<" to "<<destination<<endl;
+towerOfHanoi(n-1,auxiliary,source,destination);
+}
+int main()
+{
+    int num_disks=3;
+  towerOfHanoi(num_disks,'A','B','C');
+    return 0;
+}
