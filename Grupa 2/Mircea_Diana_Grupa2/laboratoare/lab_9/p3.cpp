d101ffbf9eb6ecb9eab395ab0a9b8dc7fb47325c
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-11-22 10:45:15 +0200

    Update p3.cpp

--- Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_9/p3.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_9/p3.cpp
@@ -1,1 +1,45 @@
-
+#include <iostream>
+using namespace std;
+//Variabila globala pentru a urmari progresul recursivitatii
+int i=0;
+//Declarare functii recursive
+void foo1(int);
+void foo2(int);
+//Definitie fundtie foo1
+void foo1(int n)
+{
+    //Verfica conditia pentru a continua recurvitatea
+    if(i<=n)
+    {
+        //Afiseaza valoarea curenta a i
+        cout<<i<<" ";
+        //implementarea valorilor lui i
+        i++;
+        //apelarea funtiei foo2, realizand astfel o recursivitate indirecta
+        foo2(n);
+    }
+    else
+        return; //incheie functia daca conditia nu este indeplinita
+}
+//definitia functiei foo2
+void foo2(int n)
+{
+    //verifica conditia pentru a continua recursivitatea
+    if(i<=n)
+    {
+        //afiseaza valoarea curenta a i
+        cout<<i << " ";
+        i++;
+        //apelarea fnuctiei foo1, realizand astfel o recursivitate indirecta
+        foo1(n);
+}
+    else
+        return;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    foo1(n);
+    return 0;
+}
