0016875e65d14fe4876e3364c1ff03c1d0cf2a05
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-11-22 10:23:33 +0200

    Update p1.cpp

--- Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_9/p1.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/laboratoare/lab_9/p1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int fact_iterativ(int n)
+{
+    int fact=1;
+    for(i=1; i<=n; i++)
+    {
+        fact*=i;
+    }
+    return fact;
+}
+
+int fact_recursiv(int n)
+{
+    //Cazul de baza pentru recursivitate: 0! este 1
+    if(n==0)
+        return 1;
+    else 
+        //Formula recursiva: n!=n*(n-1)!
+        return n*fact_recursiv(n-1);
+}
+int main()
+{
+    int n;
+    cout<<"Dati N: ;
+    cin>>n;
+    //Afisarea rezultatului pentru metoda iterativa
+    cout<<"N! cu metoda iterativa este egal cu: "<<fact_iterativ(n)<<endl;
+    //Afisarea rezultatului pentru metoda recursiva
+    cout<<"N! cu metoda recursiva este egak cu: "<<fact_recursiv(n);
+    return 0;
+}
 
