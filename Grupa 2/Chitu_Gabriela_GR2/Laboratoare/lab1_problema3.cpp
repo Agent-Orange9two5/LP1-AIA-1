e9e8d323bee53cf7ed2aef0ec58464b7682d01b3
gabrielachitu29 <147593753+gabrielachitu29@users.noreply.github.com>
2023-11-25 17:31:12 +0200

    Create lab1_problema3.cpp

---
+++ Grupa 2/Chitu_Gabriela_GR2/Laboratoare/lab1_problema3.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+
+using namespace std;
+
+void problema3()
+{
+    float c, f;
+    cout<<"Grade in Fahrenheit: ";
+    cin>>f;
+
+    c = (5.0 / 9.0) * (f - 32);
+    cout<<"Grade in Celsius: "<<c<< endl;
+    cin>> c;
+
+    return 0;
+}
