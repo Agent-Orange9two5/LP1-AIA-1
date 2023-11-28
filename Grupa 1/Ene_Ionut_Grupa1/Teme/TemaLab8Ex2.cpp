2b51a5440290ea3bfdc8d7749f5fc90e8819df0d
EneIonut09 <147584002+EneIonut09@users.noreply.github.com>
2023-11-28 15:46:37 +0200

    Create TemaLab8Ex2.cpp

---
+++ Grupa 1/Ene_Ionut_Grupa1/Teme/TemaLab8Ex2.cpp
@@ -0,0 +1,56 @@
+//Codul imi cere sa ii ofer o dimensiune array-ului, o aloca, imi cere sa ii dau de la tastatura valori de tip float ca sa ocupe spatiul alocat, dupa imi cere sa ofer o noua dimensiune pe care o aplica cu realloc si imi cere sa mai adaug valori astfel incat sa ocupe dimensiunea noua daca este cazul, iar daca dimensiuna noua este mai mica decat cea initiala scade elementele array-ului atfel incat aiba dimensiunea valorii doi, dupa care elibereaza memoria
+
+#include <iostream>
+
+
+int main() {
+    int size;
+    std::cout << "Enter the initial size of the array: ";
+    std::cin >> size;
+
+
+    float *arrMalloc = (float *)malloc(size * sizeof(float));
+    if (arrMalloc == nullptr) {
+        std::cout << "Memory allocation failed using malloc!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Memory allocated using malloc." << std::endl;
+
+    std::cout << "Enter " << size << " float numbers:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arrMalloc[i];
+    }
+
+    int newSize;
+    std::cout << "Enter the new size for reallocation: ";
+    std::cin >> newSize;
+
+
+    float * arrRealloc  = (float *)realloc(arrMalloc, newSize * sizeof(float));
+    if (arrRealloc == nullptr) {
+        std::cout << "Memory reallocation failed using realloc!" << std::endl;
+        free(arrMalloc);
+        return 1;
+    }
+
+    std::cout << "Memory reallocated using realloc." << std::endl;
+
+
+    std::cout << "Enter " << newSize - size << " more float numbers:" << std::endl;
+    for (int i = size; i < newSize; ++i) {
+        std::cin >> arrRealloc[i];
+    }
+
+
+    std::cout << "Updated array:" << std::endl;
+    for (int i = 0; i < newSize; ++i) {
+        std::cout << arrRealloc[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+    free(arrRealloc);
+
+    return 0;
+}
