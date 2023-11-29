9930914457102ea6559f6d5e9c55290d6927a52d
deliasfetcu <147593712+deliasfetcu@users.noreply.github.com>
2023-11-29 11:07:26 +0200

    Update pb1.c

--- Grupa 2/Sfetcu_Delia_g2/Laboratoare/Lab10/pb1.c
+++ Grupa 2/Sfetcu_Delia_g2/Laboratoare/Lab10/pb1.c
@@ -3,94 +3,42 @@
 
 void main()
 {
-    FILE *fp;
-    float a;
-    int i, j, m, n;
-    if ((fp=fopen("DATA.txt", "w"))==NULL)
-        printf("Eroare la deschidere DATA.txt pentru scriere\n");
+  FILE *fp;
+  float a;
+  int i,j,m,n;
+  if((fp=fopen("DATA.txt", "w"))==NULL)
+    printf("Eroare la deschidere DATA.txt pt scriere\n");
+  else{
+    printf("Introduceti umarul de linii: ");
+    scanf("%d", &n);
+    printf("Introduceti nr de coloane: ");
+    scanf("%d", &m);
+
+
+    for(i=0;i<n;i++){
+        printf("Elemente linie %d \n ", i);
+        for(j=0;j<m;j++){
+            printf("element[%d]=", j);
+            scanf("%f", &a);
+            fprintf(fp, "%.2f ", a);
+        }
+    fprintf(fp, "\n");
+  }
+  fputs("Matricea a fost scrisa cu succes", fp);
+  fclose(fp);
+  }
+  if((fp=fopen("DATA.txt", "r"))==NULL)
+    printf("Eroare la deschidere DATA.txt pt citire \n");
     else{
-        printf("Introduceti nr de linii: ");
-        scanf("%d", &n);
-        printf("Introduceti nr de coloane: ");
-        scanf("%d", &m);
-        for(i=0;i<n;i++)
-        {
-            printf("Elemente linie %d \n", i);
-            for(j=0;j<m;j++)
-            {
-                printf("Element[%d]=",j);
-                scanf("%f", &a);
-                fprintf(fp,"%.2f",a);
+        printf("Elementele matricii sunt: \n");
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+              fscanf(fp, "%f", &a);
+              printf("%.2f\t", a);
             }
-            fprintf(fp, "\n");
-        }
-        fputs("Matricea a fost scrisa cu succes!",fp);
-        fclose(fp);
-    }
-    if ((fp=fopen("DATA.txt", "r"))==NULL)
-        printf("Eroare la deschiderea DATA.txt pentru citire\n");
-    else
-    {
-        printf("Elementele matricei sunt:\n");
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<m;j++)
-            {
-                fscanf(fp,"%f",&a);
-                printf("%.2f\t",a);
-            }
-            printf("\n");
-        }
-        fclose(fp);
-    }
-    return 0;
-}
+            printf("\n");}
 
 
-/*#include <stdio.h>
 
-void main(){
-    FILE*fp;
-    float a;
-    int i,j,m,n;
-    if((fp = fopen("DATA.txt","w")) == NULL)
-        printf("Eroare la deschidere DATA.txt pentru scriere\n");
-    else
-    {
-        printf("Introduceti numarul de linii: ");
-        scanf("%d", &n);
-        printf("Introduceti numarul de coloane: ");
-        scanf("%d", &m);
 
-        for(i = 0; i < n; i++)
-        {
-            printf("Elemente linie %d \n", i);
-            for(j = 0; j < n; j++)
-            {
-                printf("Element[%d]=", j);
-                scanf("%f", &a);
-                fprintf(fp, "%.2f", a);
-            }
-            fprintf(fp, "\n");
-        }
-        fputs("Matricea a fost scrisa cu succes!", fp);
-        fclose(fp);
-     }
-     if((fp = fopen("DATA.txt", "r")) == NULL)
-        printf("Eroare la deschidere DATA.txt pentru citire\n");
-     else
-     {
-         printf("Elementele matricei sunt:\n");
-         for(i = 0; i < n; i++)
-         {
-             for(j = 0; j < n; j++)
-                {
-                    fscanf(fp, "%f", &a);
-                    printf("%.2f\t", a);
-                }
-                printf("\n");
-         }
-         fclose(fp);
-     }
-}
-*/
+    fclose(fp);}}
