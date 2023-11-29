3df5873e894b1dd5952e9e3c5e7523079eadb2a0
deliasfetcu <147593712+deliasfetcu@users.noreply.github.com>
2023-11-29 11:55:13 +0200

    Create pb4.cpp

---
+++ Grupa 2/Sfetcu_Delia_g2/Laboratoare/Lab10/pb4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    FILE *pFile;
+    char a[500] = "";
+    char linie[70];
+    int i, j;
+    int num = 0;
+    char vocale[] = "aAeEiIoOuU";
+    pFile = fopen("fisier4.txt", "r");
+    if(pFile == NULL)
+        perror("Eroare la deschidere!");
+    else
+    {
+        while(fgets(linie, 70, pFile) != NULL)
+            strcat(a, linie);
+        fclose(pFile);
+    }
+
+    for(i = 0; i < strlen(a); i++)
+    {
+        cout << a[i];
+        if(strchr(vocale, a[i]))
+        {
+            num++;
+        }
+    }
+    cout << endl << "Numarul de vocale este " << num;
+    return 0;
+}
