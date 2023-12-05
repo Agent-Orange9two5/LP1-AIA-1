f2e19d3658c76d47e8e1f286361121e9b9b73292
BinisorTeodor <147583978+BinisorTeodor@users.noreply.github.com>
2023-12-05 21:37:55 +0200

    Update TemaFisiere.cpp

--- Grupa 1/Binisor_Teodor_Gr1/TemeLaborator/TemaFisiere.cpp
+++ Grupa 1/Binisor_Teodor_Gr1/TemeLaborator/TemaFisiere.cpp
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <iostream>
 
+using namespace std;
+
+
+struct Student {
+    char nume[50];
+    int varsta;
+    double medie;
+};
+
+int main()
+{
+    FILE* file = fopen("DATA.txt", "w");
+    if (file == NULL)
+    {
+        printf("Eroare la deschiderea DATA.txt pentru scriere\n");
+    }
+    else
+    {
+        Student student[2];
+        student[0] = { "Ana Maria",19,9.50 };
+        student[1] = { "Marius Popescu",20,8.50 };
+        for (int i = 0; i < 2; i++)
+        {
+            fprintf(file, student[i].nume);
+            fprintf(file, " ");
+            fprintf(file, "%d", student[i].varsta);
+            fprintf(file, " ");
+            fprintf(file, "%.2f", student[i].medie);
+            fprintf(file, "\n");
+        }
+        fclose(file);
+        file = fopen("DATA.txt", "r");
+        while (!feof(file))
+        {
+            char c = fgetc(file);
+            printf("%c", c);
+            printf(" ");
+            printf("%ld", ftell(file));
+            printf(" ");
+        }
+    }
+    return 0;
+}
