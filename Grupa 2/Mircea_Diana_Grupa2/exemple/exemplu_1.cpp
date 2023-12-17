b0858535be33247738f3ae9a4a4d8cf18ee56bba
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-12-17 21:25:54 +0200

    Update exemplu_1.cpp

--- Grupa 2/Mircea_Diana_Grupa2/exemple/exemplu_1.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/exemple/exemplu_1.cpp
@@ -1,6 +1,6 @@
 //Exemplu 1
 /* Creați un vector cu n elemente întregi, unde n este citit de la tastatură. Inițializați vectorul cu numere întregi citite de la tastatură și calculați suma elementelor pare din vector.
-b) Scrieți suma calculată a elementelor pare într-un fișier numit "SumaPare.txt". */
+b) Scrieți suma calculată a elementelor pare într-un fișier numit "SumaPare.txt".*/
 #include <iostream>
 #include <fstream>
 using namespace std;
