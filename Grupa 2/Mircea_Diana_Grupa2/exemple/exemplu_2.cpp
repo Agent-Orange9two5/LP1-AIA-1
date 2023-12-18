f2565a3414399838eb2323634bb480f438ae4f7e
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-12-18 16:17:01 +0200

    Update exemplu_2.cpp

--- Grupa 2/Mircea_Diana_Grupa2/exemple/exemplu_2.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/exemple/exemplu_2.cpp
@@ -1,1 +1,25 @@
+/*Citirea unei matrice pătratice de dimensiune n (n ≤ 10) și calcularea sumei elementelor de pe diagonala principală și a celei secundare.
+b) Scrierea sumelor calculare într-un fișier numit "SumeDiagonale.txt".*/
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    int a[10][10],i,j,n,sp=0,ss=0;
+    cin>>n;
+    ofstream g("SumeDiagonale.txt");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++)
+    {
+        cin>>a[i][j];
+        if(i==j)
+            sp+=a[i][j];
+        if(i+j==n-1)
+            ss+=a[i][j];
+    }
+    }
+    g<<"Suma diagonalei principale "<<sp<<endl;
+    g<<"Suma diagonalei secundare "<<ss<<endl;
+    return 0;
+}
