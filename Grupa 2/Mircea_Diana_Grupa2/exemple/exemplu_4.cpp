1a0997f53b2f3d97a67f326682aa020cb8341235
Diana Mircea <147593792+DianaMircea1@users.noreply.github.com>
2023-12-18 21:34:42 +0200

    Update exemplu_4.cpp

--- Grupa 2/Mircea_Diana_Grupa2/exemple/exemplu_4.cpp
+++ Grupa 2/Mircea_Diana_Grupa2/exemple/exemplu_4.cpp
@@ -1,1 +1,13 @@
+/*Generați aleator n numere întregi și stocați-le într-un vector.
+b) Sortați aceste numere în ordine descrescătoare și afișați-le într-un fișier numit "NumereSortate.txt".*/
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main()
+{
+    int 
+    
+    return 0;
+}
