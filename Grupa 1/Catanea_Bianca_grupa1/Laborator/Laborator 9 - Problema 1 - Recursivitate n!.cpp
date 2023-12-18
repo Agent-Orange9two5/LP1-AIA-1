7c8e60b8b18832001f02a2ee7a6743e82e384274
Cătănea Bianca-Florina <147583988+cataneabianca@users.noreply.github.com>
2023-12-18 22:52:55 +0200

    Create Laborator 9 - Problema 1 - Recursivitate n!.cpp

---
+++ Grupa 1/Catanea_Bianca_grupa1/Laborator/Laborator 9 - Problema 1 - Recursivitate n!.cpp
@@ -0,0 +1,32 @@
+//Calculul lui n!
+
+#include <iostream>
+using namespace std;
+int fact_iterativ(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+int fact_recursiv(int n) {
+    // Cazul de bază pentru recursivitate: 0! este 1
+    if (n == 0)
+        return 1;
+    else
+        // Formula recursiva: n! = n * (n-1)!
+        return n * fact_recursiv(n - 1);
+}
+int main() {
+    int n;
+    cout << "Introduceti valoarea lui n: ";
+    cin >> n;
+  
+    // Afisarea rezultatului pentru metoda iterativa
+    cout << "N! cu metoda iterativa este egal cu: " << fact_iterativ(n) << endl;
+  
+    // Afisarea rezultatului pentru metoda recursiva
+    cout << "N! cu metoda recursiva este egal cu: " << fact_recursiv(n);
+  
+    return 0;
+}
