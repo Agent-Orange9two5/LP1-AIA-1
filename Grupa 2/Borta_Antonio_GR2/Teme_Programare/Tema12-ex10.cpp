5396aca3464f96a2482e898421c6c573743fd495
Antonio-Borta <147593930+Antonio-Borta@users.noreply.github.com>
2023-12-19 23:53:19 +0200

    Create Tema12-ex10.cpp

---
+++ Grupa 2/Borta_Antonio_GR2/Teme_Programare/Tema12-ex10.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, i, j, c, **a;
+    ofstream file("MatriceInmultita.txt");
+    cout << "Introduceti numarul de linii si numarul de coloane: ";
+    cin >> n >> m;
+    cout << "Introduceti constanta cu care va fii inmultita matricea: ";
+    cin >> c;
+    a = (int**)malloc(n*sizeof(int *));
+    if (a == nullptr)
+    {
+        cout << "Eroare la alocarea memoriei!" << endl;
+        return 1;
+    }
+    a[0] = (int *)malloc(n * m * sizeof(int));
+    if (a[0] == nullptr) {
+        cout << "Eroare la alocarea memoriei!" << endl;
+        free(a);
+        return 1;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        a[i] = a[0] + i * m;
+    }
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < n; j++)
+    {
+        cin >> a[i][j];
+        a[i][j] *= c;
+    }
+    for(int i = 0; i < n; ++i)
+    {  for(int j = 0; j < n; j++)
+        {
+            file << a[i][j] << " ";
+        }
+        file << endl;
+    }
+    free(a[0]);
+    free(a);
+    return 0;
+}
