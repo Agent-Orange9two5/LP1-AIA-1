466fb6805c10ca85aec23878bfb678a9a7bc0cd8
adaonu17 <147583916+adaonu17@users.noreply.github.com>
2023-12-20 17:05:05 +0200

    Create ex2.cpp

---
+++ Grupa 1/Onu_Ada_gr1/Teme/ex2.cpp
@@ -0,0 +1,33 @@
+// a) Creați un program care primește de la tastatură un număr n și apoi citește n numere întregi.
+// Alocați dinamic un vector pentru aceste numere și calculați suma lor.
+// b) Scrieți suma calculată într-un fișier numit "SumaNumere.txt".
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+ofstream g("SumaNumere.txt");
+int main()
+{
+    int n, i, s=0, *v;
+    cout << "Introduceti numarul de elemente al vectorului: ";
+    cin >> n;
+    
+    v=(int *)malloc(n*sizeof(int));
+    if(v == NULL )
+    {
+        cout << "Nu se poate aloca memorie.";
+        return -1;
+
+    }
+    
+    cout << "Introduceti elementele vectorului: ";
+    for(i=0;i<n;i++)
+    {
+        cin >> v[i];
+        s+=v[i];
+    }
+    
+    g << "Suma elementelor vectorului este: " << s;
+
+    return 0;
+}
