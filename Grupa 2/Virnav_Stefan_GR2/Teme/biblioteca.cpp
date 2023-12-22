ddd7e8e0b095ea4dd81acbbcad51cd8ce74b67ae
StefanVir <122097429+StefanVir@users.noreply.github.com>
2023-12-22 08:18:10 +0200

    Create biblioteca.cpp

---
+++ Grupa 2/Virnav_Stefan_GR2/Teme/biblioteca.cpp
@@ -0,0 +1,14 @@
+/* Să se creeze un program care gestionează o bibliotecă digitală. Programul ar trebui să utilizeze alocare dinamică pentru a gestiona diferitele cărți din bibliotecă.
+Fiecare carte trebuie să fie reprezentată de o structură de date care să conțină următoarele informații: titlu, autor, anul publicării și un identificator unic.
+
+În plus, programul ar trebui să ofere următoarele funcționalități:
+
+- Adăugarea de cărți: Utilizatorul poate introduce informațiile despre o nouă carte (titlu, autor, anul publicării) și aceasta va fi adăugată în bibliotecă.
+Identificatorul unic al cărții va fi generat în mod dinamic.
+
+- Căutarea cărților: Utilizatorul poate căuta cărți în bibliotecă după titlu, autor sau anul publicării și programul va afișa toate cărțile care corespund căutării.
+
+- Ștergerea cărților: Utilizatorul poate șterge o carte din bibliotecă, introducând identificatorul unic al cărții.
+
+- Salvarea și stergerea bibliotecii în/din fișier: Programul ar trebui să permită salvarea bibliotecii într-un fișier text și stergerea acesteia din fișier.
+*/
