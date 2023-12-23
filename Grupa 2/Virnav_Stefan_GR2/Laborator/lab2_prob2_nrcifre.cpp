9693c3eb75ea08712c9e2e9ff5297c973ee9f812
StefanVir <122097429+StefanVir@users.noreply.github.com>
2023-12-23 23:19:07 +0200

    Update lab2_prob2_nrcifre.cpp

--- Grupa 2/Virnav_Stefan_GR2/Laborator/lab2_prob2_nrcifre.cpp
+++ Grupa 2/Virnav_Stefan_GR2/Laborator/lab2_prob2_nrcifre.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   /* 2. Scrieti un program care cere utilizatorului un numar natural intreg si si va determina numarul de cifre al acestuia. */
+  
+   int ok=0,b;
+   cout << " Introduceti un numar intreg: ";
+   cin >> b ;
+   while(b>0)
+   {
+       b /=10;
+       ++ok;
+   }
+   cout << "Numarul are " << ok << " cifre.";
+    return 0;
+}
 
