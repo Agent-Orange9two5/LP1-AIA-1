291e7e5c2ffb99a56804be29b7b9fc76d758613d
StefanVir <122097429+StefanVir@users.noreply.github.com>
2023-12-23 23:37:59 +0200

    Update lab2_prob4_sir_fibonacci.cpp

--- Grupa 2/Virnav_Stefan_GR2/Laborator/lab2_prob4_sir_fibonacci.cpp
+++ Grupa 2/Virnav_Stefan_GR2/Laborator/lab2_prob4_sir_fibonacci.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+
+using namespace std;
+void fib(int n)
+{
+  int i=3,a=1,b=1,c;
+  while(i<=n)
+  {
+    c=a+b;
+    a=b;
+    b=c;
+    i++;
+    cout<<c<<" ";
+  }
+}
+int main()
+{
+     // 4.	Scrieti un program care realizeaza generarea a sirului lui Fi
+     int n;
+     cout << " Pentru cate elemente se genereaza sirul Fibonacci? ";
+     cin>>n;
+     cout << "Sirul Fibonacci: "<< 1 << " " << 1 << " ";
+     fib(n);
+     
+    return 0;
+}
 
