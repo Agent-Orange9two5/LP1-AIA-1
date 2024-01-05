130434bf6cf3dbab7544c8599ab41b27996eba5a
Virnav Stefan-Adrian <122097429+StefanVir@users.noreply.github.com>
2024-01-05 19:08:51 +0200

    Update Criptare&Decriptare.cpp

--- Grupa 2/Virnav_Stefan_GR2/Extras/Criptare&Decriptare.cpp
+++ Grupa 2/Virnav_Stefan_GR2/Extras/Criptare&Decriptare.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
 
+
+/*Programul utilizeaza o varianta modificata a algoritmului lui  Vigenère pentru a cripta si decripta 
+texte formate din litere mari si cifre*/
+int main()
+{
+
+}
