a6071fef8e1cd4a1b593203e3f0897d793f8b926
Virnav Stefan-Adrian <122097429+StefanVir@users.noreply.github.com>
2024-01-09 13:31:33 +0200

    Update Criptare&Decriptare.cpp

--- Grupa 2/Virnav_Stefan_GR2/Extras/Criptare&Decriptare.cpp
+++ Grupa 2/Virnav_Stefan_GR2/Extras/Criptare&Decriptare.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <string>
+
 
 /*Programul utilizeaza o varianta modificata a algoritmului lui  Vigenère pentru a cripta si decripta
 texte formate din litere mari si cifre*/
 class Matrix
 {
 public:
-    char matrice[36][36];
+    std::vector<std::string> matrice;
     //Constructor
     Matrix()
     {
-        char a[36][36];
-        //Generare matrice
-        strcpy(a[0],"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
-        strcpy(matrice[0],a[0]);
+    std::string a="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for(int i=0;i<36;i++)
+        matrice.push_back(a);
     }
 };
 int main()
 {
-
+        Matrix a;
 }
