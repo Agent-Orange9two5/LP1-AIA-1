cd621e62d396b23ee70053366ba82292565416b2
Virnav Stefan-Adrian <122097429+StefanVir@users.noreply.github.com>
2024-01-09 12:54:15 +0200

    Update Criptare&Decriptare.cpp

--- Grupa 2/Virnav_Stefan_GR2/Extras/Criptare&Decriptare.cpp
+++ Grupa 2/Virnav_Stefan_GR2/Extras/Criptare&Decriptare.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cstring>
 
-
-/*Programul utilizeaza o varianta modificata a algoritmului lui  Vigenère pentru a cripta si decripta 
+/*Programul utilizeaza o varianta modificata a algoritmului lui  Vigenère pentru a cripta si decripta
 texte formate din litere mari si cifre*/
+class Matrix
+{
+public:
+    char matrice[36][36];
+    //Constructor
+    Matrix()
+    {
+        char a[36][36];
+        //Generare matrice
+        strcpy(a[0],"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+        strcpy(matrice[0],a[0]);
+    }
+};
 int main()
 {
 
