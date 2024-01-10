0ff55e9ab9fa70597c67a6565a0814dbc5bbaf0a
Iustin Mărgărit <147593770+Iustin-Margarit@users.noreply.github.com>
2024-01-10 20:16:49 +0200

    Update a.cpp

--- Grupa 2/Margarit_Iustin_Gr2/Laboratoare/a.cpp
+++ Grupa 2/Margarit_Iustin_Gr2/Laboratoare/a.cpp
@@ -2,12 +2,8 @@
 #include <fstream>
 using namespace std;
 
-
 struct orase{
-string denumire,indice,locuitori,suprafata;
-
-};
-
+string denumire,indice,suprafata,locuitori;};
 
 void citire(orase v[],int n){
 int i,j;
@@ -36,39 +32,33 @@ for(i=0;i<n;i++)
     getline(cin,v[i].indice);
 }
 
-for(i=0;i<n;i++)
-    for(j=0;i<n;j++)
-        H[i][j]=v[i].denumire;
-
 }
 
 int fisier(orase v[],int n){
 ofstream g("Orase.txt");
-int i,j;
+int i,j,k,l;
 string s[n];
 
 
- for(i=0;i<n;i++){
+ for(i=0;i<n;i++)
    s[i]=v[i].denumire+", "+v[i].locuitori+", "+v[i].suprafata+", "+v[i].indice;
-
-cout<<s[0];
+   
   for(i=0;i<n-1;i++)
-    for(j=i+1;j<n;j++)
-    if(v[i].locuitori>v[j].locuitori)
-      {string aux=s[i];
-        s[i]=s[j];
-        s[j]=aux;}
+    {k=stoi(v[i].locuitori);
+        for(j=i+1;j<n;j++)
+         {l=stoi(v[j].locuitori);
+            if(k>l)
+          {string aux=s[i];
+           s[i]=s[j];
+           s[j]=aux;}}}
 
     for(i=0;i<n;i++)
     {g<<s[i]<<endl;
     cout<<s[i]<<endl;}
 
+    return 0;
+
  }
-return 0;
-}
-
-
-
 
 
 int main()
