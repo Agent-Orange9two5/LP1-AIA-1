87f4136bef7488a82c84742b6f5bc7b86ff716ed
Ciobanu Constantin <118921743+Fresh03@users.noreply.github.com>
2024-01-25 22:51:08 +0200

    Create prog1.cpp

---
+++ Grupa 1/Ciobanu_Constantin_GR1/Laborator/Lab 9/prog1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int fact_iterativ(int n)
+{
+    int fact = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int fact_recursiv(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact_recursiv(n - 1);
+    }
+}
+
+
+int main()
+{
+    int n;
+    cout << "Dati n: ";
+    cin >> n;
+    cout << "N! cu metoda iterativa este egal cu: " << fact_iterativ(n) << endl;
+    cout << "N! cu metoda recursiva este egal cu: " << fact_recursiv(n) << endl;
+    return 0;
+}
